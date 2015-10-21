cd215d7dafb7600295ad11b8ec46ef822d4151a8
Kei Sawada <k@swd.cc>
2015-10-21 21:48:30 +0900

    Fix an error in :telnet mode - server fails to start whenever label contains double quote character

--- lib/burn/fuel/telnet/scene.rb
+++ lib/burn/fuel/telnet/scene.rb
@@ -6,7 +6,7 @@ module Burn
         BG = 40          # background palette (0x00-0x0f)
         TEXT = 30        # background palette (0x00-0x0f)
         # SPRITE = 17     # sprite palette (0x10-0x1f)
-        
+
         # Color Control
         WHITE = 7
         LIGHTBLUE = 4
@@ -23,7 +23,7 @@ module Burn
         BLUEGREEN = 6
         GRAY = 7
         BLACK = 0
-        
+
         # Light Control
         DARKEST = 60
         DARKER = 60
@@ -33,23 +33,23 @@ module Burn
         def initialize(resource_name, context)
           super(resource_name, context)
         end
-        
+
         def label(string, x=0, y=1)
-          @context.instance_exec { @opcodes << "@screen.display[#{y}][#{x},#{string.length}] = \"#{string}\"" }
+          @context.instance_exec { @opcodes << "@screen.display[#{y}][#{x},#{string.length}] = \"#{string.gsub(/"/, '"')}\"" }
         end
 
         def wait(interval)
           @context.instance_exec { @opcodes << "@wait = #{interval}" }
         end
-        
+
         def method_missing(method_symbol, *args, &block)
           #TBD if needed
         end
-        
+
         def goto(scene_name)
           @context.instance_exec { @opcodes << "@screen.flush_screen && @pc = @opcodes.index(\"##{scene_name}\")" }
         end
-        
+
         def color(palette, color, lightness=:lighter)
           target = palette==:bg ? "bg" : "fg"
           palette=Scene.const_get(palette.upcase)
@@ -57,7 +57,7 @@ module Burn
           lightness=Scene.const_get(lightness.upcase)
           @context.instance_exec { @opcodes << "@screen.#{target}_color = #{palette+color+lightness}" }
         end
-        
+
         def main_loop(rrb_source=nil)
           @context.instance_exec(@context,@resource_name) do |c, resource|
             p=Pxes::CrubyTranspiler.new(Ripper.sexp(rrb_source),c,resource)
@@ -68,11 +68,11 @@ module Burn
             @opcodes << "@pc = @opcodes.index(\"#{start_label}\")" #JUMP
           end
         end
-        
+
         def sprite(resource)
           @context.instance_exec { @opcodes << "@screen.activated_sprite_objects << @___#{resource} if @screen.activated_sprite_objects.index(@___#{resource}).nil?" }
         end
-        
+
         def inline(code)
           @context.instance_exec { @opcodes << code }
         end
@@ -85,31 +85,31 @@ module Burn
 #                ].each {|p| @code_blocks.push p}
 #              end
 #            end
-#            
+#
 #            def fade_out(sec=3)
 #              @context.instance_exec {@code_blocks.push "screen_fade_out(#{sec.to_s});"}
 #            end
 #
 #            def show(string, x=0, y=0, *args)
 #            end
-#            
+#
 #            def inline(code)
 #            end
-#            
+#
 #            def screen(map, vars)
 #            end
-#            
+#
 #            def paint(dest, palette)
 #            end
-#            
+#
 #            def rand
 #              @context.instance_exec {@code_blocks.push "rand8()"}
 #            end
-#            
+#
 #            # utility methods
 #            def range(x_from, y_from, x_to, y_to)
 #            end
-        
+
       end
     end
   end
